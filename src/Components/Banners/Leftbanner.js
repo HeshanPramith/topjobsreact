1077cdff6f6b388b4cdd35b6408208b4a95569e1
heshan@genesiis.com <heshan@genesiis.com>
2023-01-13 16:23:48 +0530

    second commit

--- src/Components/Banners/Leftbanner.js
+++ src/Components/Banners/Leftbanner.js
@@ -7,6 +7,19 @@ import Slider from "react-slick";
 
 function Leftbanner() {
 
+    const settings2 = {
+        className: "left-banner",
+        dots: false,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 4000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        cssEase: "linear",
+      };
+
   const settings = {
     className: "left-banner",
     dots: false,
@@ -22,18 +35,39 @@ function Leftbanner() {
 
   return (
     <div id='main-banner'>
-        <Slider {...settings}>
-        <div>
-            <div className='d-flex align-items-center justify-content-end tpbanner'>
-                <img src='images/left_banner/160x600d.jpg' className='img-fluid' alt='banner'></img>
-            </div>
+        <div className='d-block d-sm-none'>
+            <Slider {...settings2}>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/left_banner/mobile/bannerinfom2.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/left_banner/mobile/bannerinfom4.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/left_banner/mobile/bannerinfom.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+            </Slider>
         </div>
-        <div>
-            <div className='d-flex align-items-center justify-content-end tpbanner'>
-                <img src='images/left_banner/bannerinfo.png' className='img-fluid' alt='banner'></img>
-            </div>
+        <div className='d-none d-sm-block'>
+            <Slider {...settings}>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/left_banner/160x600d.jpg' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/left_banner/bannerinfo.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+            </Slider>
         </div>
-        </Slider>
     </div>
   );
 }
--- src/Components/Banners/Right banner.js
+++ src/Components/Banners/Right banner.js
@@ -7,6 +7,19 @@ import Slider from "react-slick";
 
 function Rightbanner() {
 
+    const settings2 = {
+        className: "right-banner",
+        dots: false,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 4000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        cssEase: "linear",
+      };
+
   const settings = {
     className: "right-banner",
     dots: false,
@@ -22,23 +35,44 @@ function Rightbanner() {
 
   return (
     <div id='main-banner'>
-        <Slider {...settings}>
-        <div>
-            <div className='d-flex align-items-center justify-content-end tpbanner'>
-                <img src='images/right_banner/000000100.png' className='img-fluid' alt='banner'></img>
-            </div>
+        <div className='d-block d-sm-none'>
+            <Slider {...settings2}>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/right_banner/mobile/bannerinfom2.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/right_banner/mobile/bannerinfom4.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/right_banner/mobile/bannerinfom.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+            </Slider>
         </div>
-        <div>
-            <div className='d-flex align-items-center justify-content-end tpbanner'>
-                <img src='images/right_banner/000000300.png' className='img-fluid' alt='banner'></img>
-            </div>
+        <div className='d-none d-sm-block'>
+            <Slider {...settings}>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/right_banner/000000100.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/right_banner/000000300.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex align-items-center justify-content-end tpbanner'>
+                        <img src='images/right_banner/bannerinfo2.png' className='img-fluid' alt='banner'></img>
+                    </div>
+                </div>
+            </Slider>
         </div>
-        <div>
-            <div className='d-flex align-items-center justify-content-end tpbanner'>
-                <img src='images/right_banner/bannerinfo2.png' className='img-fluid' alt='banner'></img>
-            </div>
-        </div>
-        </Slider>
     </div>
   );
 }
