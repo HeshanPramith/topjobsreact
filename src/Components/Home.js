a497c2627ebfba36951dafe1c1b3624cde512d56
heshan@genesiis.com <heshan@genesiis.com>
2023-05-23 17:10:56 +0530

    Welcome page added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -27,31 +27,29 @@ function Home () {
                         Sorry, your browser doesn't support videos.
                     </video>
                 </div>
-                <h1 className='logotxt'>topjobs <span>recruitment made easy</span></h1>
-                <Container fluid>
-                    <Row>
-                        <Col xs={12} className='px-0 text-center'>
-                            <div className='main-search'>
-                                <h1>Recruitment Made Easy</h1>
-                                <Form className='search-form'>
-                                    <InputGroup className="mb-2">
-                                        <Form.Control
-                                            placeholder="Job title, keywords, or company"
-                                            aria-label="Job title, keywords, or company"
-                                            aria-describedby="basic-addon2"
-                                            size="lg"
-                                        />
-                                        <Button variant="primary" type="submit" size="lg" id="button-addon2">
-                                            <Link to='/Topjobs'>Quick Search</Link>
-                                        </Button>
-                                    </InputGroup>
-                                </Form><br></br>
-                                <span>OR</span><br></br>
-                                <Link className='gotobtn' to='/Topjobs'>Go To Job Page</Link>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+                
+                <div className='main-search-wrap main-search-wrap d-flex flex-column align-items-center justify-content-center'>
+                    <p className='welcmtxt'>Welcome to</p>
+                    <h1 className='logotxt'>topjobs</h1>
+                    <div className='main-search'>
+                        <h1>Recruitment Made Easy</h1>
+                        <Form className='search-form'>
+                            <InputGroup className="mb-2">
+                                <Form.Control
+                                    placeholder="IT-Sware"
+                                    aria-label="Job title, keywords, or company"
+                                    aria-describedby="basic-addon2"
+                                    size="lg"
+                                />
+                                <Button variant="primary" type="submit" size="lg" id="button-addon2">
+                                    <Link to='/vacancybyfunctionalarealist'>Quick Search</Link>
+                                </Button>
+                            </InputGroup>
+                        </Form><br></br>
+                        <span>OR</span><br></br>
+                        <Link className='gotobtn' to='/Topjobs'>Go To Job Page</Link>
+                    </div>
+                </div>
                 <div className='infogr-para'>
                     <p><b>topjobs Sri Lanka Job Network</b>most popular online job site in Sri Lanka for jobs, careers, recruitment and employment with recruitment automation for employers.</p>
                 </div>
