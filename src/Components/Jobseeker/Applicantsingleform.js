2bb592380b90b56ca346c0c5f68e1f4baa7a04e3
heshan@genesiis.com <heshan@genesiis.com>
2023-05-24 19:48:46 +0530

    Common profile ui added v4

--- src/Components/Jobseeker/Applicantsingleform.js
+++ src/Components/Jobseeker/Applicantsingleform.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import Table from 'react-bootstrap/Table';
+// import Table from 'react-bootstrap/Table';
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Jsheader from '../Common/Jsheader';
